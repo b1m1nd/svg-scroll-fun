654f7dacfc5aba77a45a388b75d86bfa52de06a8
b1mind <brent@1mindsolutions.com>
2021-02-17 01:11:18 -0700

    🧙‍♂️ teh way

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -16,52 +16,40 @@ const circles = document.querySelector('.circles').children
 const dot = document.querySelector('#dot')
 const viewBox = document.querySelector('#viewBox')
 const contentHeight = document.querySelector('.contentWrap').clientHeight
-const PATH_TL = gsap.timeline()
 const clientHeight = document.body.clientHeight
 
 console.log(contentHeight)
-gsap.to(dot, {
-  scale: 0.5,
-  transformOrigin: 'center',
-  ease: 'none',
-  motionPath: { path: '#path', align: '#dot' },
-  scrollTrigger: {
-    trigger: '#viewBox',
-    pin: true,
-    start: 'top top',
-    end: `${contentHeight}`,
-    scrub: 1,
-    markers: true,
-  },
+
+const pathTl = gsap
+  .timeline()
+  .from(dot, {
+    duration: 0.03,
+    scale: 0.2,
+    autoAlpha: 0,
+    transformOrigin: 'center',
+  })
+  .to(dot, { motionPath: { path: '#path', align: '#dot' } })
+  .to(dot, { duration: 0.2, scale: 5 }, '<')
+  .to(dot, { duration: 0.2, scale: 1 }, '-=0.2')
+
+ScrollTrigger.create({
+  trigger: viewBox,
+  animation: pathTl,
+  start: 'center center',
+  end: contentHeight,
+  pin: true,
+  scrub: 1,
 })
 
-gsap.from('.welcome p', {
-  autoAlpha: 0,
-  x: 100,
-  stagger: 0.5,
-  scrollTrigger: {
-    trigger: '.welcome',
-    pin: true,
-    start: 'center center',
-    scrub: 1,
-  },
-})
+const contentTl = gsap
+  .timeline()
+  .from('.welcome p', { autoAlpha: 0, x: 100, stagger: 0.3 })
 
-gsap.from('.second p', {
-  autoAlpha: 0,
-  x: -100,
-  stagger: 0.5,
-  scrollTrigger: {
-    trigger: '.second',
-    pin: true,
-    start: 'center center',
-    scrub: 1,
-  },
-})
-
-dot.addEventListener('click', e => {
-  console.dir(e.target)
-  console.log()
-  gsap.to(viewBox, { attr: { viewBox: '260 0 100 100' } })
-  gsap.to(e.target, { scale: 1 })
+ScrollTrigger.create({
+  trigger: '.contentWrap',
+  animation: contentTl,
+  start: 'top top',
+  end: 'bottom center',
+  pin: true,
+  scrub: 1,
 })
