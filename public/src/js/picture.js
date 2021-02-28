ca5333f55bdbb15c38f6905e3d1dbab0b1cc51c6
b1mind <brent@1mindsolutions.com>
2021-02-28 02:12:00 -0700

    😞 so close but so far

--- public/src/js/picture.js
+++ public/src/js/picture.js
@@ -8,6 +8,9 @@ const front = document.querySelector('.front')
 const mid = document.querySelector('.mid')
 const far = document.querySelector('.far')
 
+gsap.set(sun, { transformOrigin: 'center center' })
+gsap.to(sun, { duration: 10, rotation: 360, repeat: -1, ease: 'none' })
+
 const zoomTl = gsap
   .timeline({
     defaults: {
@@ -17,20 +20,21 @@ const zoomTl = gsap
   .to(
     '.pictureBox',
     {
-      attr: { viewBox: '250 250 200 260' },
+      attr: { viewBox: '250 150 200 260' },
     },
     0,
   )
-  .to(sun, { scale: 0.25, x: -25, y: 125 }, 0)
-  .to(far, { scale: 0.35, x: 270, y: 50 }, 0)
-  .to(mid, { scale: 1.35, x: 50, y: -150 }, 0)
+  .to(sun, { scale: 0.25, x: -45, y: 15 }, 0)
+  .to(far, { scale: 0.25, x: 850, y: 165 }, 0)
+  .to(mid, { scale: 1.35, x: -175, y: -150 }, 0)
   .to(front, { scale: 1, x: -100 }, 0)
 
 ScrollTrigger.create({
   trigger: '.picture-grid',
   animation: zoomTl,
   start: 'top top',
-  end: 'bottom top',
+  end: 'bottom+=1000 top',
   pin: true,
   scrub: 1,
+  markers: true,
 })
